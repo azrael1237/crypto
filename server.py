5f70819541c6e5a6f7fdf803df5fbdc02310f18a
azrael1237 <amkamfskfsk@seznam.cz>
2025-06-13 08:29:11 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,115 +1,63 @@
-from tinydb import TinyDB, Query
-from aiohttp import web
-import socketio
+import socket
+import threading
+import queue
 
+# Configuration
+HOST = "10.0.1.40"
+PORT = 8080
 
-db = TinyDB('user_keys.json')
-db.truncate()
+class ChatServer:
+    def __init__(self, host, port):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((host, port))
+        self.server.listen(2)
+        print(f"[SERVER] Listening on {host}:{port}")
+        self.clients = []
+        self.nicknames = {}
+        self.msg_queue = queue.Queue()
 
-def create_user_schema(username, ik, sik, spk, spk_sign):
-    return {"username": username, "ik": ik, "sik": sik, "spk": spk, "spk_sign": spk_sign}
+    def client_thread(self, client, addr, client_idx):
+        if len(self.clients) == 1:
+            client.sendall(b"Initializing chat...\n")
+            client.sendall(b"Waiting for peer...\n")
+        else:
+            # Notify both clients
+            for c in self.clients:
+                c.sendall(b"Peer connected! You can chat now.\n")
+        while True:
+            try:
+                data = client.recv(4096)
+                if not data:
+                    break
+                msg = data.decode('utf-8')
+                self.msg_queue.put((client_idx, msg))
+            except Exception as e:
+                print(f"[ERROR] {e}")
+                break
+        client.close()
+        self.clients.remove(client)
+        print(f"[DISCONNECT] Client {addr} disconnected")
 
+    def broadcaster(self):
+        while True:
+            idx, msg = self.msg_queue.get()
+            sender = self.clients[idx]
+            for i, c in enumerate(self.clients):
+                if c != sender:
+                    try:
+                        c.sendall(msg.encode('utf-8'))
+                    except Exception as e:
+                        print(f"[ERROR] {e}")
 
-def find_user(username):
-    query = Query()
-    res = db.search(query.username == username)
-    if len(res):
-        return (True, res[0])
-    else:
-        return (False, None)
+    def run(self):
+        threading.Thread(target=self.broadcaster, daemon=True).start()
+        client_idx = 0
+        while True:
+            client, addr = self.server.accept()
+            self.clients.append(client)
+            print(f"[CONNECT] {addr} connected.")
+            threading.Thread(target=self.client_thread, args=(client, addr, client_idx), daemon=True).start()
+            client_idx += 1
 
-
-def update_user(username, user):
-    query = Query()
-    db.update(user, query.username == username)
-
-
-def add_user(username, ik, sik, spk, spk_sign):
-    user_json = create_user_schema(username, ik, sik, spk, spk_sign)
-    if (find_user(username)[0]):
-        update_user(username, user_json)
-    else:
-        db.insert(user_json)
-
-def update_user_spk(username, spk, spk_sign):
-    res = find_user(username)
-    if (not res[0]):
-        raise Exception(f"User {username} not found!")
-
-    user = res[1]
-
-    res['spk'] = spk
-    res['spk_sign'] = spk_sign
-
-    update_user(username, user)
-
-
-def request_prekey(username):
-    res = find_user(username)
-    if (not res[0]):
-        raise Exception(f"User {username} not found!")
-
-    user = res[1]
-
-    return {"ik": user['ik'], "sik": user['sik'], "spk": user['spk'], "spk_sign": user['spk_sign']}
-
-sio = socketio.AsyncServer(logger=True, engineio_logger=True)
-app = web.Application()
-
-sio.attach(app)
-
-user_map = {}
-sid_map = {}
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
-
-@sio.on('pongi')
-async def pongi(sid, data):
-    await sio.emit("pingi", "sankalp")
-    return True
-@sio.on('register_user')
-async def on_register_user(sid, data):
-    user_map[data["username"]] = sid
-    sid_map[sid] = data["username"]
-    add_user(data["username"], data["ik"],  data["sik"], data["spk"], data["spk_sig"])
-    return True
-
-@sio.on('request_users')
-async def on_request_users(sid):
-    return list(user_map.keys())
-
-@sio.on('request_prekey')
-async def on_request_prekey(sid, data):
-    try:
-        prekey_bundle = request_prekey(data["username"])
-    except:
-        return (False, {})
-    return (True, prekey_bundle)
-
-@sio.on('x3dh_message')
-async def on_x3dh_message(sid, data):
-    if not data['username'] in user_map:
-        return False
-    
-    res = await sio.call('x3dh_message', data, sid=user_map[data['username']])
-    return res
-
-
-@sio.on('ratchet_msg')
-async def on_ratchet_msg(sid, data):
-    if not data['username'] in user_map:
-        return False
-
-    res = await sio.call('ratchet_msg', data, sid=user_map[data['username']])
-    return res
-
-@sio.event
-async def disconnect(sid):
-    print('disconnect')
-    if sid in sid_map and sid_map[sid] in user_map:
-        del user_map[sid_map[sid]]
-        del sid_map[sid]
-if __name__ == '__main__':
-
-    web.run_app(app)
+if __name__ == "__main__":
+    ChatServer(HOST, PORT).run()
